aeb6d07fb177e129b54c560af5f4dca453ad37d9
Stefan Kleff <stefan.kleff@crosslend.com>
2023-01-10 09:27:14 +0100

    codestyle

--- src/Service/MyService.php
+++ src/Service/MyService.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-
 use Doctrine\ORM\EntityManagerInterface;
 
 class MyService
 {
+
     public function __construct(EntityManagerInterface $entityManager) {}
 
 }
